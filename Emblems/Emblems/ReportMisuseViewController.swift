46f71d18d74219da69e817eb53e3c7eea0bd104e
Adam Corcoran <adam_corcoran@me.com>
2015-06-18 18:15:26 +1000

    Revert 2a8e5e2..4f6e271
    This rolls back to commit 2a8e5e21ebaa1322c34900b4368cebcff34edaf7.

--- Emblems/Emblems/ReportMisuseViewController.swift
+++ Emblems/Emblems/ReportMisuseViewController.swift
@@ -12,85 +12,12 @@ import MobileCoreServices
 
 class ReportMisuseViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    var activeField:UITextField?
-    
     @IBOutlet weak var imageView: UIImageView!
-    
-    @IBOutlet weak var scrollViewKeyboardScrollControl: UIScrollView!
     
 //    override func viewDidLoad() {
 //        let view = self.view as! UIScrollView
 //        view.contentSize = CGSize(width: self.view.frame.size.width, height: 600)
 //    }
-    
-    @IBOutlet weak var contentViewSizing: UIView!
-    
-    override func viewDidLoad() {
-//        NSLayoutConstraint *leftConstraint = [NSLayoutConstraint constraintWithItem:self.contentView
-//            attribute:NSLayoutAttributeLeading
-//            relatedBy:0
-//            toItem:self.view
-//            attribute:NSLayoutAttributeLeft
-//            multiplier:1.0
-//            constant:0];
-//        [self.view addConstraint:leftConstraint];
-        let leftConstraint = NSLayoutConstraint(item: self.contentViewSizing, attribute: NSLayoutAttribute.Leading, relatedBy: .Equal, toItem: self.view, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: 0)
-        self.view.addConstraint(leftConstraint)
-        
-        let rightConstraint = NSLayoutConstraint(item: self.contentViewSizing, attribute: NSLayoutAttribute.Trailing, relatedBy: .Equal, toItem: self.view, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: 0)
-        self.view.addConstraint(rightConstraint)
-        
-//        NSLayoutConstraint *rightConstraint = [NSLayoutConstraint constraintWithItem:self.contentView
-//            attribute:NSLayoutAttributeTrailing
-//            relatedBy:0
-//            toItem:self.view
-//            attribute:NSLayoutAttributeRight
-//            multiplier:1.0
-//            constant:0];
-//        [self.view addConstraint:rightConstraint];
-        
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardDidShow:"), name: UIKeyboardDidShowNotification, object: nil)
-        
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillBeHidden"), name: UIKeyboardWillHideNotification, object: nil)
-    }
-    
-    @IBAction func textFieldDidBeginEditing(sender:UITextField) {
-        self.activeField = sender;
-    }
-    
-    @IBAction func textFieldDidEndEditing(sender:UITextField) {
-        self.activeField = nil;
-    }
-    
-//    func keyboardDidShow(notification: NSNotification) {
-//        var info = notification.userInfo
-//        var kbRect =
-//    }
-    
-    
-    - (void) keyboardDidShow:(NSNotification *)notification
-    {
-    NSDictionary* info = [notification userInfo];
-    CGRect kbRect = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
-    kbRect = [self.view convertRect:kbRect fromView:nil];
-    
-    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbRect.size.height, 0.0);
-    self.scrollView.contentInset = contentInsets;
-    self.scrollView.scrollIndicatorInsets = contentInsets;
-    
-    CGRect aRect = self.view.frame;
-    aRect.size.height -= kbRect.size.height;
-    if (!CGRectContainsPoint(aRect, self.activeField.frame.origin) ) {
-    [self.scrollView scrollRectToVisible:self.activeField.frame animated:YES];
-    }
-    }
-    
-    - (void) keyboardWillBeHidden:(NSNotification *)notification
-    {
-    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
-    self.scrollView.contentInset = contentInsets;
-    self.scrollView.scrollIndicatorInsets = contentInsets;
-    }
     
     
 //    func makeRoomForImage() {
