bff2f585a203dd97018d5c72623fdb70df7e3804
Adam Corcoran <adam_corcoran@me.com>
2015-06-18 20:39:50 +1000

    Alamofire installed
    Added Alamofire to handle network calls.

--- Emblems/Pods/Target Support Files/Pods-Emblems/Pods-Emblems-frameworks.sh
+++ Emblems/Pods/Target Support Files/Pods-Emblems/Pods-Emblems-frameworks.sh
@@ -49,11 +49,13 @@ code_sign() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'AFNetworking.framework'
+  install_framework 'Alamofire.framework'
   install_framework 'SMTPAPI.framework'
   install_framework 'SendGrid.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'AFNetworking.framework'
+  install_framework 'Alamofire.framework'
   install_framework 'SMTPAPI.framework'
   install_framework 'SendGrid.framework'
 fi
