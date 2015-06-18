fa38345be4f9522e65d04acf41307848f71a0a16
Adam Corcoran <adam_corcoran@me.com>
2015-06-18 18:03:03 +1000

    Dead-end on Obj-C to Swift translation (for now)
    Have begun trying to translate Objective-C code to Swift but have found
    Swift code instead. Will try using that.

--- Emblems/Emblems/ReportMisuseViewController.swift
+++ Emblems/Emblems/ReportMisuseViewController.swift
@@ -67,7 +67,13 @@ class ReportMisuseViewController: UIViewController, UIImagePickerControllerDeleg
 //        var kbRect =
 //    }
     
+    // Problematic translation of Objective-C to Swift
+    func keyboardDidShow(notification: NSNotification) {
+        let info : NSDictionary = notification.userInfo
+        let kbRect : CGRect = info.objectForKey(UIKeyboardFrameBeginUserInfoKey)
+    }
     
+    // Objective-C code
     - (void) keyboardDidShow:(NSNotification *)notification
     {
     NSDictionary* info = [notification userInfo];
